536968c76fa027cd38f558309310b3e8c1fe455d
guo-yong-zhi <55872791+guo-yong-zhi@users.noreply.github.com>
2021-08-27 20:09:52 +0800

    阻止屏幕自动关闭且按电源键关闭filebrowser服务

---
+++ filebrowser/show_status.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+test_process=$(ps aux | grep "[f]ilebrowser")
+if [ -n "$test_process" ]
+then
+    result="File Browser is Running....."
+else
+    result="File Browser is not running."
+fi
+eips 25 2 "$result"
---
+++ filebrowser/start.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+lipc-set-prop -i com.lab126.powerd preventScreenSaver 1
+lipc-wait-event  com.lab126.hal powerButtonPressed| if read event; then
+    lipc-set-prop -i com.lab126.powerd preventScreenSaver 0
+    killall filebrowser
+    eips 25 2 "File Browser is turned off. "
+fi > /dev/null &
+
+iptables -I INPUT -p tcp --dport 80 -j ACCEPT
+eips 25 1 "IP: `ifconfig wlan0 | grep 'inet addr' | awk -F '[ :]' '{print $13}'`"
+
+if [ "$(ps aux | grep '[f]ilebrowser')" ];then killall filebrowser;fi
+
+if [ ! -f ./filebrowser.db ]
+then
+    ./filebrowser --noauth -a 0.0.0.0 -p 80 -r /mnt/us > /dev/null
+else
+    METHOD=$(./filebrowser config cat | grep 'Auth method' | awk '{print $3}')
+    if [ 'noauth' != "$METHOD" ];
+    then
+        ./filebrowser config set --auth.method=noauth > /dev/null
+    fi
+    ./filebrowser -a 0.0.0.0 -p 80 -r /mnt/us > /dev/null
+fi
