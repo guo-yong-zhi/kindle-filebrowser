b13a085c97e68f14efd9003435f8933c6af30d5f
guo-yong-zhi <55872791+guo-yong-zhi@users.noreply.github.com>
2022-05-02 17:00:44 +0800

    fix display residue after kill

---
+++ filebrowser/filebrowser_kill.sh
@@ -0,0 +1,6 @@
+lipc-send-event com.lab126.hal powerButtonPressed
+sleep 5
+ps aux | grep [f]ilebrowser | awk '{print $2}' | xargs -i kill {} > /dev/null
+lipc-set-prop -i com.lab126.powerd preventScreenSaver 0
+eips 30 1 "                     "
+eips 30 3 "                              "
