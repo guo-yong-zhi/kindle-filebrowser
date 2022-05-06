aedf0b783ba0cbeab8714fcb704420f3f47b8b30
guo-yong-zhi <55872791+guo-yong-zhi@users.noreply.github.com>
2022-05-06 21:50:40 +0800

    automatically close the service when the WLAN off

--- filebrowser_wrapper.sh
+++ filebrowser_wrapper.sh
@@ -2,21 +2,25 @@
 col=30
 iptables -I INPUT -p tcp --dport 80 -j ACCEPT
 # enable wireless if it is currently off
-WIFI_IS_OFF=0
+WIFI_WAS_OFF=0
 if [ 0 -eq `lipc-get-prop com.lab126.cmd wirelessEnable` ]; then
 	eips $col 3 "WiFi is off, turning it on now"
 	lipc-set-prop com.lab126.cmd wirelessEnable 1
-	WIFI_IS_OFF=1
+	WIFI_WAS_OFF=1
 fi
 
 # start filebrowser in the background
 if [ "$(ps aux | grep '[f]ilebrowser')" ];then killall filebrowser;fi
 "$@" &
 
-# refresh IP display in the background
+# refresh IP display and monitor wlan status in the background
 while :; do
     eips $col 1 "== `ifconfig wlan0 | grep 'inet addr' | awk -F '[ :]' '{print $13}'` =="
-    sleep 5
+    sleep 3
+    if [ 0 -eq `lipc-get-prop com.lab126.cmd wirelessEnable` ]; then
+    	lipc-send-event com.lab126.hal powerButtonPressed
+    	break
+    fi
 done > /dev/null &
 
 # waiting for powerButtonPressed
@@ -27,7 +31,7 @@ kill $(jobs -p)
 eips $col 3 "File Browser is turned off.   "
 
 # Restore WiFi status
-if [ 1 -eq $WIFI_IS_OFF ]; then
+if [ 1 -eq $WIFI_WAS_OFF ] && [ 1 -eq `lipc-get-prop com.lab126.cmd wirelessEnable` ]; then
 	lipc-set-prop com.lab126.cmd wirelessEnable 0
 	sleep 0.5
 	eips $col 3 "Turning off WiFi              "
